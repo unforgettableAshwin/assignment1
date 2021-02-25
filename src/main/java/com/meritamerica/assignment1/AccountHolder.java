cb3f2ea318c9fcd27ffb5f1b245c078fdfeb6881
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-02-25 00:49:41 -0500

    Inital revision.

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -1,5 +1,67 @@
 package com.meritamerica.assignment1;
 
-public class AccountHolder {
-	
+public class AccountHolder
+{
+	private AccountHolder()
+	{
+	}
+
+	private AccountHolder( String firstName, String middleName, String lastName, String ssn,
+			double checkingAccountOpeningBalance, double savingsAccountOpeningBalance )
+	{
+	}
+
+	public String getFirstName()
+	{
+		return "";
+	}
+
+	public void setFirstName( String firstName )
+	{
+
+	}
+
+	public String getMiddleName()
+	{
+		return "";
+	}
+
+	public void setMiddleName( String middleName )
+	{
+	}
+
+	public String getLastName()
+	{
+		return "";
+	}
+
+	public void setLastName( String lastName )
+	{
+	}
+
+	public String getSSN()
+	{
+		return "";
+	}
+
+	public void setSSN( String ssn )
+	{
+	}
+
+	public CheckingAccount getCheckingAccount()
+	{
+		return new CheckingAccount();
+	}
+
+	public SavingsAccount getSavingsAccount()
+	{
+		return new SavingsAccount();
+	}
+
+	public String toString()
+	{
+		StringBuilder s = new StringBuilder( "Name: " + getFirstName() + " " + getMiddleName() + " " + getLastName() );
+
+		return s.toString();
+	}
 }
